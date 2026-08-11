4ec19228b4d8b374924cfccdc6abe6c9b81552fb
Elewartor <ele33war@gmail.com>
2026-08-11 23:04:33 +0000

    quota fix

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "kubernetes_namespace_v1" "this" {
 resource "kubernetes_resource_quota_v1" "pod_quota" {
   metadata {
     name      = "${var.name}-pod-quota"
-    namespace = kubernetes_namespace.my_namespace.metadata[0].name
+    namespace = kubernetes_namespace_v1.this.metadata[0].name
   }
 
   spec {
