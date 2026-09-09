8943239319dffa681d9c4189dbf0bef5b0ce177a
Elewartor <ele33war@gmail.com>
2026-09-09 01:19:11 +0000

    v1 support added

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "kubernetes_resource_quota_v1" "this" {
 }
 
 
-resource "kubernetes_limit_range" "this" {
+resource "kubernetes_limit_range_v1" "this" {
   metadata {
     name = "${var.name}-limit-range"
     namespace = kubernetes_namespace_v1.this.metadata[0].name
